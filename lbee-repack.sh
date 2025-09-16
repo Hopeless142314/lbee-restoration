35ed9d7a53fe7afb82822c1b52f3bbfb1b4dbd19
Kristiyan Georgiev <2003kristiyan@gmail.com>
2025-09-16 22:17:02 +0200

    Update the shell script to handle script paks

--- lbee-repack.sh
+++ lbee-repack.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 <path>"
+  echo "Usage: $0 /path/to/game/folder/"
   exit 1
 fi
 
-input_path="$1"
-output_path="./output"
-lucksystem_bin="./lucksystem"
+input_path=$1
+output_path=./output
+lucksystem_bin=./lucksystem
+lb_repack_path=./LB_repack
 
 if [ ! -f "$lucksystem_bin" ]; then
   echo "Error: $lucksystem_bin not found!"
@@ -21,33 +22,65 @@ fi
 
 if [ ! -f "$input_path/LITBUS_WIN32.exe" ]; then
   echo "Error: $input_path/LITBUS_WIN32.exe not found!"
-  echo 
-  echo "Double-check that the path leads to the game's root directory."
-  echo "Example: ~/.local/share/Steam/steamapps/common/Little Busters!\ English\ Edition"
+  echo "Double-check and make sure that the path leads to the game's root directory."
   exit 1
 fi
 
-mkdir -p "$output_path"
 mkdir -p "$output_path/files"
-cp "./source/system.cnf" "$output_path/"
+cp ./source/system.cnf "$output_path/"
 
 repack() {
-    file="$1"
-    pak=${file^^}.PAK
+  file="$1"
+  pak=$(echo "$file" | tr '[:lower:]' '[:upper:]').PAK
 
-    if [ ! -f "$input_path/files/$pak" ]; then
-        echo "Error: $input_path/files/$pak not found!"
-        exit 1
-    fi
+  if [ ! -f "$input_path/files/$pak" ]; then
+      echo "Error: $input_path/files/$pak not found!"
+      exit 1
+  fi
 
-    echo "Processing $pak..."
+  echo "Processing $pak..."
 
-    mkdir -p "$output_path/$file-temp"
-    $lucksystem_bin "pak" "extract" -s "$input_path/files/$pak" -i "$input_path/files/$pak" -o "$output_path/temp" -a "$output_path/$file-temp"
-    rm "$output_path/temp"
-    cp -r "./source/$file-done/" "$output_path/$file-temp/"
-    $lucksystem_bin "pak" "replace" -s "$input_path/files/$pak" -i "$output_path/$file-temp" -o "$output_path/files/$pak"
-    rm -r "$output_path/$file-temp"
+  mkdir -p "$output_path/$file-temp"
+  "$lucksystem_bin" pak extract -s "$input_path/files/$pak" -i "$input_path/files/$pak" -o "$output_path/temp" -a "$output_path/$file-temp" > /dev/null 2>&1
+  rm "$output_path/temp"
+  cp -r ./source/$file-done/* "$output_path/$file-temp/"
+  "$lucksystem_bin" pak replace -s "$input_path/files/$pak" -i "$output_path/$file-temp" -o "$output_path/files/$pak" > /dev/null 2>&1
+  rm -r "$output_path/$file-temp"
+}
+
+rewrite() {
+  file=$1
+  pak=$(echo "$file" | tr '[:lower:]' '[:upper:]').PAK
+
+  if [ ! -d "$lb_repack_path" ]; then
+      echo "Warning: $lb_repack_path not found! Skipping $pak"
+      return
+  fi
+
+  if ! command -v python3 &> /dev/null; then
+      echo "Warning: python3 not installed! Skipping $pak"
+      return
+  fi
+
+  if [ ! -f $input_path/files/$pak ]; then
+      echo "Error: $input_path/files/$pak not found!"
+      exit 1
+  fi
+
+  echo "Processing $pak..."
+
+  cd $lb_repack_path
+  mkdir -p ./core
+  mkdir -p ./SCRIPT
+  mv ./steam/*.py ./ > /dev/null 2>&1
+  mv ./steam/core/opcode_steam.txt ./core/opcode_steam.txt > /dev/null 2>&1
+  cp $input_path/files/$pak ./SCRIPT/SCRIPT_steam.PAK
+
+  python3 unpack.py > /dev/null 2>&1
+  cp -r ../source/script-done/* ./SCRIPT/disassembled/
+  python3 repack.py > /dev/null 2>&1
+  mv ./SCRIPT/SCRIPT_repacked.PAK ../$output_path/files/$pak
+  cd ../
 }
 
 repack "battle"
@@ -55,10 +88,12 @@ repack "bgcg"
 repack "charcg"
 repack "eventcg"
 repack "gencg"
+repack "gm"
 repack "othcg"
 repack "parts"
 repack "pt"
 repack "syscg"
+rewrite "script"
 
-echo "Repacking completed! Check the '$output_path' directory for results."
+echo "Patching completed! Check the '$output_path' directory for the results."
 exit 0
