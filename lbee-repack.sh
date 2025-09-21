1ad9f33213f08a20ebfe863d7a3d0ba6c8ef0cdc
Kristiyan Georgiev <2003kristiyan@gmail.com>
2025-09-21 21:37:13 +0200

    Add censored assets

--- lbee-repack.sh
+++ lbee-repack.sh
@@ -1,14 +1,56 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 /path/to/game/folder/"
-  exit 1
-fi
+output_path="./output"
+lucksystem_bin="./lucksystem"
+use_censored=false
 
-input_path=$1
-output_path=./output
-lucksystem_bin=./lucksystem
-lb_repack_path=./LB_repack
+show_help() {
+  echo "Usage: $0 [options] /path/to/game/folder/"
+  echo
+  echo "Options:"
+  echo "  -c, --censor          Patch using the censored assets (default: false)"
+  echo "  -o, --output DIR      Specify output directory        (default: ./output)"
+  echo "  -l, --lucksystem PATH Specify LuckSystem binary path  (default: ./lucksystem)"
+  echo "  -h, --help            Show this help message"
+  echo
+  exit 1
+}
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -o|--output)
+      output_path="$2"
+      shift 2
+      ;;
+    -l|--lucksystem)
+      lucksystem_bin="$2"
+      shift 2
+      ;;
+    -c|--censor)
+      use_censored=true
+      shift
+      ;;
+    -h|--help)
+      show_help
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      show_help
+      ;;
+    *)
+      if [ -z "$input_path" ]; then
+        input_path="$1"
+      else
+        echo "Error: Multiple input paths specified"
+      fi
+      shift
+      ;;
+  esac
+done
+
+if [ -z "$input_path" ]; then
+  show_help
+fi
 
 if [ ! -f "$lucksystem_bin" ]; then
   echo "Error: $lucksystem_bin not found!"
@@ -22,7 +64,7 @@ fi
 
 if [ ! -f "$input_path/LITBUS_WIN32.exe" ]; then
   echo "Error: $input_path/LITBUS_WIN32.exe not found!"
-  echo "Double-check and make sure that the path leads to the game's root directory."
+  echo "Make sure that the path leads to the game's root directory."
   exit 1
 fi
 
@@ -37,8 +79,18 @@ cp ./source/auxiliary-files/movie/OP00_en.webm "$output_path/files/movie/OP00_en
 cp ./source/auxiliary-files/movie/OP00.webm "$output_path/files/movie/OP00.webm"
 cp ./source/auxiliary-files/movie/EDAL_en.webm "$output_path/files/movie/low/EDAL_en.webm"
 cp ./source/auxiliary-files/movie/EDAL.webm "$output_path/files/movie/low/EDAL.webm"
-cp ./source/auxiliary-files/movie/OP00_og.webm "$output_path/files/movie/low/OP00_en.webm"
+cp ./source/auxiliary-files/movie/OP00_og_en.webm "$output_path/files/movie/low/OP00_en.webm"
 cp ./source/auxiliary-files/movie/OP00_og.webm "$output_path/files/movie/low/OP00.webm"
+
+if [ "$use_censored" = true ]; then
+  echo "Processing censored assets..."
+
+  rm -r ./source/eventcg-done/
+  cp ./source/auxiliary-files/clean/eventcg-done ./source/eventcg-done
+  cp ./source/auxiliary-files/clean/SEEN0520 ./source/script-done/SEEN0520
+  cp ./source/auxiliary-files/clean/NYKD_MASK01 ./source/othcg-done/NYKD_MASK01
+fi
+
 
 repack() {
   file="$1"
@@ -54,44 +106,9 @@ repack() {
   mkdir -p "$output_path/$file-temp"
   "$lucksystem_bin" pak extract -s "$input_path/files/$pak" -i "$input_path/files/$pak" -o "$output_path/temp" -a "$output_path/$file-temp" > /dev/null 2>&1
   rm "$output_path/temp"
-  cp -r ./source/$file-done/* "$output_path/$file-temp/"
+  cp -r "./source/$file-done/"* "$output_path/$file-temp/"
   "$lucksystem_bin" pak replace -s "$input_path/files/$pak" -i "$output_path/$file-temp" -o "$output_path/files/$pak" > /dev/null 2>&1
   rm -r "$output_path/$file-temp"
-}
-
-rewrite() {
-  file=$1
-  pak=$(echo "$file" | tr '[:lower:]' '[:upper:]').PAK
-
-  if [ ! -d "$lb_repack_path" ]; then
-      echo "Warning: $lb_repack_path not found! Skipping $pak"
-      return
-  fi
-
-  if ! command -v python3 &> /dev/null; then
-      echo "Warning: python3 not installed! Skipping $pak"
-      return
-  fi
-
-  if [ ! -f $input_path/files/$pak ]; then
-      echo "Error: $input_path/files/$pak not found!"
-      exit 1
-  fi
-
-  echo "Processing $pak..."
-
-  cd $lb_repack_path
-  mkdir -p ./core
-  mkdir -p ./SCRIPT
-  mv ./steam/*.py ./ > /dev/null 2>&1
-  mv ./steam/core/opcode_steam.txt ./core/opcode_steam.txt > /dev/null 2>&1
-  cp $input_path/files/$pak ./SCRIPT/SCRIPT_steam.PAK
-
-  python3 unpack.py > /dev/null 2>&1
-  cp -r ../source/script-done/* ./SCRIPT/disassembled/
-  python3 repack.py > /dev/null 2>&1
-  mv ./SCRIPT/SCRIPT_repacked.PAK ../$output_path/files/$pak
-  cd ../
 }
 
 repack "battle"
@@ -104,7 +121,7 @@ repack "othcg"
 repack "parts"
 repack "pt"
 repack "syscg"
-rewrite "script"
+repack "script"
 
 echo "Patching completed! Check the '$output_path' directory for the patched files."
 exit 0
